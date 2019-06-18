99cb792c62b1b7bb0526995bf12e86cc8fe97e85
Jean M. Girard <Jean.M.Girard@Outlook.com>
2019-06-17 21:37:35 -0400

    removed api key

--- src/app/modules/social/mock-social.ts
+++ src/app/modules/social/mock-social.ts
@@ -2,14 +2,17 @@ import { Social } from './social';
 
 export const SOCIALS: Social[] = [
     { 
+        ref: 'fb',
         name: 'Facebook',
         link: 'https://goo.gl/maps/VS3aiRYsAGVwmhvR9',
         img: '/assets/img/social/instagram-96.png'
     },{
+        ref: 'ggl',
         name: 'Google',
         link: 'https://goo.gl/maps/VS3aiRYsAGVwmhvR9',
         img: '/assets/img/social/google-96.png'
     },{
+        ref: 'insta',
         name: 'Instagram',
         link: 'https://goo.gl/maps/VS3aiRYsAGVwmhvR9',
         img: '/assets/img/social/instagram-96.png'
--- src/app/modules/social/social.service.ts
+++ src/app/modules/social/social.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Social } from './social';
+import { SOCIALS } from './mock-social';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SocialService {
+  private socials: Social[];
 
-  constructor() { }
+  constructor() {
+    this.socials = SOCIALS;
+   }
+
+
+  public getFacebook(): Social{
+    for(const acc of this.socials){
+      if(acc.ref === 'fb'){ return acc; }
+    }
+    return null;
+  }
 }
