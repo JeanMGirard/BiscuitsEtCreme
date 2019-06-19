957f14530fad3e04f682acabb1c27914d1719cd1
Jean M. Girard <Jean.M.Girard@Outlook.com>
2019-06-18 21:20:11 -0400

    Auto-generated commit

---
+++ assets/js/app.1.js
@@ -0,0 +1,14 @@
+import './vendor';
+import { clearScreenDown } from 'readline';
+window.Vue = require('vue');
+
+//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+require("./components/header");
+/*const app = new Vue({
+    el: '#app'
+});*/
+
+var scrollDown = function(){
+    var hgt = screen.height;
+    
+}   
---
+++ assets/js/pages/home.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function () {
+    //(new CircleType(document.getElementById('home1-title'))).radius(1200);
+    //$("#home-content-1").addClass("loaded"); 
+    (new CircleType(document.getElementById("row-1-intro"))).radius(1200);
+    $("body").addClass("loaded");
+});
