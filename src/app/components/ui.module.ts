7388169b4ec405ca3eb58b304b2421c91933205b
Jean M. Girard <Jean.M.Girard@Outlook.com>
2019-06-20 16:09:31 -0400

    finalizing

--- src/app/components/ui.module.ts
+++ src/app/components/ui.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 /** */
-import { HeaderComponent } from './header/header.component';
+// import { HeaderComponent } from './header/header.component';
 import { HeaderService } from './header/header.service';
 
 @NgModule({
   declarations: [
-    HeaderComponent
+  //  HeaderComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    HeaderComponent
+  //  HeaderComponent
   ],
   providers:[
     HeaderService
--- src/app/pages/home/parallax1/parallax1.component.ts
+++ src/app/pages/home/parallax1/parallax1.component.ts
@@ -16,7 +16,7 @@ export class Parallax1Component implements OnInit {
 
   ngOnInit() {
     $(document).ready(() => {
-      (new CircleType(document.getElementById('row-1-intro'))).radius(1200);
+      // (new CircleType(document.getElementById('row-1-intro'))).radius(1200);
       $('#row-1-intro').addClass('loaded');
     });
   }
